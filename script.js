4dc0158e6e2f72e8d098a49789e994dc862a9ada
Unmesh Bari <unmeshbari5@gmail.com>
2025-07-28 23:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function isSameType(value1, value2) {
-	if(value1 == NaN && value2 == Number){
+    if (Number.isNaN(value1) && typeof value2 === "number") {
         return false;
     }
-	if(typeof value1 == typeof value2) return true;
+    if (typeof value1 === typeof value2) return true;
     else return false;
-	
 }
-
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
+
+// Convert inputs to numbers if they are numeric
+
+if (!isNaN(value1)) value1 = Number(value1);
+if (!isNaN(value2)) value2 = Number(value2);
+
 alert(isSameType(value1, value2));
