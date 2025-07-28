e0a4845f1864d089579280a0515c4d3c2d6fbf71
Unmesh Bari <unmeshbari5@gmail.com>
2025-07-28 23:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function isSameType(value1, value2) {
-  //your js code here
+	if(typeof value1 == typeof value2) return true;
+    else return false;
 }
+
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
