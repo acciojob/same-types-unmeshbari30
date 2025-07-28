cc1485022390e8e178e515dc48897ea506609dc4
Unmesh Bari <unmeshbari5@gmail.com>
2025-07-28 23:46:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function isSameType(value1, value2) {
-	if(typeof value1 == typeof value2) return true;
-    else return false;
+    return typeof value1 === typeof value2;
 }
 
+// Convert the input properly before comparison
+let input1 = prompt("Enter Start of the Range.");
+let input2 = prompt("Enter End Of the Range.");
 
-// do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
+// Helper function to parse input like number, NaN, boolean etc.
+function parseInput(input) {
+    if (input.toLowerCase() === "nan") return NaN;
+    else if (input.toLowerCase() === "true") return true;
+    else if (input.toLowerCase() === "false") return false;
+    else if (!isNaN(Number(input))) return Number(input);
+    else return input;
+}
+
+let value1 = parseInput(input1);
+let value2 = parseInput(input2);
+
 alert(isSameType(value1, value2));
